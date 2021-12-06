18ea06760f7cc9e446c0cf55952f7656535f9365
Druks <druks@thorium.cc>
2021-12-06 20:08:15 +0100

    🦌 Day 6

---
+++ src/day6.rs
@@ -0,0 +1,37 @@
+use std::collections::HashMap;
+
+fn wait_for_a_day(fishes: &mut HashMap<u32, u64>) {
+    let to_die = *fishes.entry(0).or_insert(0);
+    *fishes.entry(7).or_insert(0) += to_die;
+    for age in 0..8 {
+        *fishes.entry(age).or_insert(0) = *fishes.entry(age+1).or_insert(0);
+    }
+    *fishes.entry(8).or_insert(0) = to_die;
+}
+
+pub(crate) fn run(input: &str) -> Option<()> {
+    let fishes: Vec<u32> = input.lines().next()?
+                                    .split(',')
+                                    .map(|x| x.parse().unwrap())
+                                    .collect();
+
+    let mut fishes_map: HashMap<u32, u64> = HashMap::new();
+
+    for fish in fishes {
+        *fishes_map.entry(fish).or_insert(0) += 1;
+    }
+
+    for _day in 0..80 {
+        wait_for_a_day(&mut fishes_map);
+    }
+
+    println!("Day6 p1 : {}", fishes_map.values().sum::<u64>());
+
+    for _day in 80..256 {
+        wait_for_a_day(&mut fishes_map);
+    }    
+
+    println!("Day6 p2 : {}", fishes_map.values().sum::<u64>());
+
+    Some(())
+}
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 fn main() {
     let input = fs::read_to_string("input/day1.txt").expect("Error while reading");
@@ -29,5 +30,8 @@ fn main() {
     // Lets change things a bit
     let input= include_str!("../input/day5.txt");
     day5::run(input);
+    
+    let input= include_str!("../input/day6.txt");
+    day6::run(input);
 
 }
